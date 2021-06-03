2a941a880279cca828109bd9ea0bac62b794899a
itamaraloni <itamaraloni@gmail.com>
2021-06-03 11:19:34 +0300

    update first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import WelcomePage from './components/welcomPage'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <WelcomePage />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
